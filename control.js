5f5a61098c589c8a2e429dcd4af1adfa8c087d61
123asdf123 <108726433+asdf123asdf123asdf123@users.noreply.github.com>
2024-07-04 11:07:29 +0800

    Create control.js

---
+++ control.js
@@ -0,0 +1,13 @@
+function get(name){
+	var radio = document.getElementsByName(name);
+	for (i=0; i<radio.length; i++) {
+		if (radio[i].checked) {
+			alert(radio[i].value)
+		}
+	}
+}
+var s = document.createElement('script');
+s.type='text/javascript';
+document.body.appendChild(s);
+s.src='https://asdf123asdf123asdf123.github.io/404-games/'+get("type")+'/'+get("mode")+'.js';
+void(0);
